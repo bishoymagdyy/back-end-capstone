5cab58bcd20e8e0e4cffc829fa7799645a272911
bishoymagdyy <Bishoy.magdy.22230293@commerce.helwan.edu.eg>
2025-10-08 02:43:21 +0300

    after api test

--- littlelemon/urls.py
+++ littlelemon/urls.py
@@ -29,9 +29,9 @@ router.register(r'users', views.UserViewSet, basename='user')
 urlpatterns = [
     path('', index),
     path('admin/', admin.site.urls),
-    path('restaurant/', include('restaurant.urls')),
-    path('restaurant/menu/',include('restaurant.urls')),
-    path('restaurant/booking/', include(router.urls)),
+    path('api/', include('restaurant.urls')),
+    path('api/menu/',include('restaurant.urls')),
+    path('api/booking/', include(router.urls)),
     path('auth/', include('djoser.urls')),
     path('auth/', include('djoser.urls.authtoken')),
     path('users/', include(router.urls)),
